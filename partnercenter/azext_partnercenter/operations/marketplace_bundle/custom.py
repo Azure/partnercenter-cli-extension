f043e1eace134bbaec02dcde53c8db6cbc663f31
Bob Jacobs <bob.jacobs@outlook.com>
2022-09-23 15:10:24 -0400

    Bobjac/dev (#18)
    * moved command to az partnercenter marketplace bundle
    
    * updating container image to include new arm-ttk release and adding happy path test data
    
    * resolving issues with the docker call
    
    * moving to _cnab_util.py
    
    * adding requirements.txt file
    
    * moving utils and cleanup
    
    * changing container_image to latest microsoft container.  this will support buildbundle but not verify
    
    * reverting to bobjac/cnab:8.0 until verify command is released in mcr.microsoft.com/container-package-app:latest

--- partnercenter/azext_partnercenter/operations/marketplace_bundle/custom.py
+++ partnercenter/azext_partnercenter/operations/marketplace_bundle/custom.py
@@ -11,6 +11,7 @@ import time
 import docker
 from azure.cli.core.util import sdk_no_wait
 from knack.util import CLIError
+from ._cnab_util import call_verify, call_buildbundle, _print_result, _get_container_id, _login_to_acr, _get_acr_name
 
 # API Operations
 # pylint: disable=too-many-locals
@@ -21,17 +22,11 @@ def verify_bundle(manifest_file):
 
 def build_bundle(manifest_file):
     container_id = _get_container_id(manifest_file)
-    result = call_buildbundle(container_id, './cpaMount')
+    acr_name = _get_acr_name(manifest_file)
+    result = _login_to_acr(container_id, acr_name)
+    result = call_buildbundle(container_id, './cpalocal/cpaMount')
     _print_result(result)
     
-def call_verify(container_id, directory):
-    command = "docker exec " + container_id + " cpa verify --directory " + directory
-    return subprocess.run([command],shell=True,capture_output=True)
-
-def call_buildbundle(container_id, directory):
-    command = "docker exec " + container_id + " cpa buildbundle --directory " + directory
-    return subprocess.run([command],shell=True,capture_output=True)
-
 def update_bundle(cmd, instance, arg):
     # TODO: Implement partnercenter marketplace offer update
     return instance
@@ -45,46 +40,9 @@ def get_bundle(cmd):
 def list_bundle(cmd):
     raise CLIError('TODO: Implement `partnercenter marketplace offer show`')
 
-def _print_result(result):
-    output = result.stdout.decode("utf-8")
-    error = result.stderr.decode("utf-8")
-    print(output)
 
-def _get_container_id(manifest_file):
-    container_name = "cpacontainer"
-    container_lookup_name = "/" + container_name
-    container_id  = _get_running_container_id(container_lookup_name)
 
-    if container_id == "":
-        mount_path = _get_mount_path(manifest_file)
-        result = _start_container(container_name, mount_path)
-        container_id = _get_running_container_id(container_lookup_name)
-    
-    return container_id
 
-def _get_running_container_id(container_name):
-    client = docker.APIClient(base_url='unix://var/run/docker.sock')
-    ps = client.containers(False, True)
-    for item in ps:
-        if item['Names'][0] == container_name:
-            return item['Id']
-    return ""
-
-def _start_container(container_name, mount_path):
-    container_image = "bobjac/cnab:7.0"
-    command = "docker run --name " + container_name + " -d -v /var/run/docker.sock:/var/run/docker.sock -v " + mount_path + ":/cpaMount " + container_image + " sleep infinity"
-    return subprocess.run([command],shell=True,capture_output=True)
-
-def _stop_container(container_name):
-    command = "docker stop " + container_name
-    return subprocess.run([command],shell=True,capture_output=True)
-
-def _remove_container(container_name):
-    command = "docker rm " + container_name
-    return subprocess.run([command],shell=True,capture_output=True)
-
-def _get_mount_path(manifest_file):
-    return os.path.dirname(manifest_file)
 
 
 
