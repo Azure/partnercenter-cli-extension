7e40f7b2a7ef0c4f43699b2f5fbd4b5122510902
Bob Jacobs <bob.jacobs@outlook.com>
2022-09-26 21:01:38 -0400

    Bobjac/dev (#20)
    * moved command to az partnercenter marketplace bundle
    
    * updating container image to include new arm-ttk release and adding happy path test data
    
    * resolving issues with the docker call
    
    * moving to _cnab_util.py
    
    * adding requirements.txt file
    
    * moving utils and cleanup
    
    * changing container_image to latest microsoft container.  this will support buildbundle but not verify
    
    * reverting to bobjac/cnab:8.0 until verify command is released in mcr.microsoft.com/container-package-app:latest
    
    * starting refactor to docker api
    
    * fix merge conflict
    
    * Moving from subcommand to docker api

--- partnercenter/azext_partnercenter/operations/marketplace_bundle/_cnab_util.py
+++ partnercenter/azext_partnercenter/operations/marketplace_bundle/_cnab_util.py
@@ -6,13 +6,21 @@ import time
 import docker
 import yaml
 
-def call_verify(container_id, directory):
-    command = "docker exec " + container_id + " cpa verify --directory " + directory
-    return subprocess.run([command],shell=True,capture_output=True)
+def verify(manifest_file):
+    container = _get_container(manifest_file)
+    result = container.exec_run('cpa verify --directory ./cpaMount')
+    return result
 
-def call_buildbundle(container_id, directory):
-    command = "docker exec -w /cpaMount " + container_id + " cpa buildbundle"
-    return subprocess.run([command],shell=True,capture_output=True)
+def bundle(manifest_file):
+    container = _get_container(manifest_file)
+    acr_name = _get_acr_name(manifest_file)
+    result = container.exec_run('az acr login -n ' + acr_name)
+    result = container.exec_run('cpa buildbundle', workdir='/cpaMount')
+    return result
+
+def _print_container_result(result):
+    output = result.output.decode("utf-8")
+    print(output)
 
 def _print_result(result):
     error = result.stderr.decode("utf-8")
@@ -22,59 +30,27 @@ def _print_result(result):
     output = result.stdout.decode("utf-8")
     print(output)
 
-def _get_running_container_id(container_name):
-    client = docker.APIClient(base_url='unix://var/run/docker.sock')
-    ps = client.containers(False, True)
-    for item in ps:
-        if item['Names'][0] == container_name:
-            return item['Id']
-    return ""
-
-def _get_container_id(manifest_file):
+def _get_container(manifest_file):
+    client = docker.from_env()
     container_name = "cpacontainer"
-    container_lookup_name = "/" + container_name
-    container_id  = _get_running_container_id(container_lookup_name)
-
-    if container_id == "":
+    try:
+        container = client.containers.get(container_name)
+        return container
+    except:
         mount_path = _get_mount_path(manifest_file)
-        result = _start_container(container_name, mount_path)
-        container_id = _get_running_container_id(container_lookup_name)
-    
-    return container_id
+        container = _run_container(container_name, mount_path)
+        return container
 
-def _start_container(container_name, mount_path):
-    container_image = "bobjac/cnab:8.0"
-    #container_image = "mcr.microsoft.com/container-package-app:latest"
-    command = "docker run --name " + container_name + " -d -v /var/run/docker.sock:/var/run/docker.sock -v " + mount_path + ":/cpaMount" + " -v $HOME/.azure:/root/.azure "+ container_image + " sleep infinity"
-    print(command)
-    return subprocess.run([command],shell=True,capture_output=True)
-
-def _stop_container(container_name):
-    command = "docker stop " + container_name
-    return subprocess.run([command],shell=True,capture_output=True)
-
-def _remove_container(container_name):
-    command = "docker rm " + container_name
-    return subprocess.run([command],shell=True,capture_output=True)
+def _run_container(container_name, mount_path):
+    client = docker.from_env()
+    home_directory = os.path.expanduser('~')
+    absolute_path = f'{home_directory}/.azure'
+    volumes = ['/var/run/docker.sock:/var/run/docker.sock', mount_path+':/cpaMount', absolute_path+':/root/.azure']
+    container = client.containers.run('bobjac/cnab:8.0', 'sleep infinity', detach=True, volumes=volumes, name=container_name)
+    return container
 
 def _get_mount_path(manifest_file):
     return os.path.dirname(manifest_file)
-
-def _make_local_directory(container_id, directory):
-    command = "docker exec " + container_id + " mkdir " + directory
-    return subprocess.run([command],shell=True,capture_output=True)
-
-def _list_directory(container_id, directory):
-    command = "docker exec " + container_id + " ls -la " + directory
-    return subprocess.run([command],shell=True,capture_output=True)
-
-def _copy_to_local_directory(container_id, directory):
-    command = "docker exec " + container_id + " cp -R /cpaMount " + directory
-    return subprocess.run([command],shell=True,capture_output=True)
-
-def _login_to_acr(container_id, acr_name):
-    command = "docker exec " + container_id + " az acr login -n " + acr_name
-    return subprocess.run([command],shell=True,capture_output=True)
 
 def _get_acr_name(manifest_file):
     acr_name = ""
--- partnercenter/azext_partnercenter/operations/marketplace_bundle/custom.py
+++ partnercenter/azext_partnercenter/operations/marketplace_bundle/custom.py
@@ -11,21 +11,17 @@ import time
 import docker
 from azure.cli.core.util import sdk_no_wait
 from knack.util import CLIError
-from ._cnab_util import call_verify, call_buildbundle, _print_result, _get_container_id, _login_to_acr, _get_acr_name
+from ._cnab_util import bundle, verify, _print_container_result
 
 # API Operations
 # pylint: disable=too-many-locals
 def verify_bundle(manifest_file):
-    container_id = _get_container_id(manifest_file)
-    result = call_verify(container_id, './cpaMount')
-    _print_result(result)
+    result = verify(manifest_file)
+    _print_container_result(result)
 
 def build_bundle(manifest_file):
-    container_id = _get_container_id(manifest_file)
-    acr_name = _get_acr_name(manifest_file)
-    result = _login_to_acr(container_id, acr_name)
-    result = call_buildbundle(container_id, './cpalocal/cpaMount')
-    _print_result(result)
+    result = bundle(manifest_file)
+    _print_container_result(result)
     
 def update_bundle(cmd, instance, arg):
     # TODO: Implement partnercenter marketplace offer update
