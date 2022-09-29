f05cc7ab654522b20b78af04c928d76d336c71a4
Bob Jacobs <bob.jacobs@outlook.com>
2022-09-29 18:21:26 -0400

    parsing output from cnab for templates

--- partnercenter/azext_partnercenter/operations/marketplace_bundle/_cnab_util.py
+++ partnercenter/azext_partnercenter/operations/marketplace_bundle/_cnab_util.py
@@ -1,15 +1,17 @@
 import os
 import os.path
-import re
-import subprocess
-import time
+import sys
 import docker
 import yaml
+import json
+from knack.util import CLIError
 
 def verify(manifest_file):
     container = _get_container(manifest_file)
     result = container.exec_run('cpa verify --directory ./cpaMount')
-    return result
+    response = _parse_verify_response(result)
+    templates = response['templates']
+    return response
 
 def bundle(manifest_file):
     container = _get_container(manifest_file)
@@ -17,6 +19,32 @@ def bundle(manifest_file):
     result = container.exec_run('az acr login -n ' + acr_name)
     result = container.exec_run('cpa buildbundle', workdir='/cpaMount')
     return result
+
+def _parse_verify_response(response):
+    parsed_template_result = _parse_template_results(response)
+    mapped_templates = list(map(lambda x: json.loads(x), parsed_template_result['templates']))
+
+    parsed_response = {
+        'templates': mapped_templates
+    }
+
+    return parsed_response
+
+def _parse_template_results(response):
+    import regex
+    output = response.output.decode("utf-8")
+    pattern = regex.compile(r'\{(?:[^{}]|(?R))*\}')
+    returned_list = pattern.findall(output)
+
+    for json_string in returned_list:
+        output = output.replace(json_string, "")
+
+    print(output)
+    parsed_results = {
+        'stripped': output,
+        'templates': returned_list
+    }
+    return parsed_results
 
 def _print_container_result(result):
     output = result.output.decode("utf-8")
@@ -35,18 +63,23 @@ def _get_container(manifest_file):
     container_name = "cpacontainer"
     try:
         container = client.containers.get(container_name)
-        return container
-    except:
+
+    except docker.errors.NotFound:
         mount_path = _get_mount_path(manifest_file)
         container = _run_container(container_name, mount_path)
+    except:
+        raise CLIError('There was an unknow error when trying to find the cpacontainer')
+    finally:
         return container
 
 def _run_container(container_name, mount_path):
     client = docker.from_env()
     home_directory = os.path.expanduser('~')
     absolute_path = f'{home_directory}/.azure'
-    volumes = ['/var/run/docker.sock:/var/run/docker.sock', mount_path+':/cpaMount', absolute_path+':/root/.azure']
-    container = client.containers.run('bobjac/cnab:8.0', 'sleep infinity', detach=True, volumes=volumes, name=container_name)
+    img = 'bobjac/cnab:8.0'
+    cmd = 'sleep infinity'
+    volumes = ['/var/run/docker.sock:/var/run/docker.sock', f'{mount_path}:/cpaMount', f'{absolute_path}:/root/.azure']
+    container = client.containers.run(img, cmd, detach=True, volumes=volumes, name=container_name)
     return container
 
 def _get_mount_path(manifest_file):
@@ -54,7 +87,7 @@ def _get_mount_path(manifest_file):
 
 def _get_acr_name(manifest_file):
     acr_name = ""
-    with open(manifest_file, 'r') as file:
+    with open(manifest_file, 'r', encoding="utf-8") as file:
         manifest = yaml.safe_load(file)
         registry_server = manifest['registryServer']
         index = registry_server.index('.')
