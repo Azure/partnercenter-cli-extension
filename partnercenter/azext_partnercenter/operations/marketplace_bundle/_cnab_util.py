4c3f60831c8f47b060d7fa5488eebd3253ce7b69
Bob Jacobs <bob.jacobs@outlook.com>
2022-09-30 09:23:50 -0400

    adding parsing

--- partnercenter/azext_partnercenter/operations/marketplace_bundle/_cnab_util.py
+++ partnercenter/azext_partnercenter/operations/marketplace_bundle/_cnab_util.py
@@ -9,9 +9,10 @@ from knack.util import CLIError
 def verify(manifest_file):
     container = _get_container(manifest_file)
     result = container.exec_run('cpa verify --directory ./cpaMount')
-    response = _parse_verify_response(result)
-    templates = response['templates']
-    return response
+    #response = _parse_verify_response(result)
+
+    #return response
+    return result.output
 
 def bundle(manifest_file):
     container = _get_container(manifest_file)
@@ -21,14 +22,35 @@ def bundle(manifest_file):
     return result
 
 def _parse_verify_response(response):
-    parsed_template_result = _parse_template_results(response)
-    mapped_templates = list(map(lambda x: json.loads(x), parsed_template_result['templates']))
+    parsed_response = {}
 
-    parsed_response = {
-        'templates': mapped_templates
-    }
+    parsed_template_result = _parse_template_results(response)
+
+    if parsed_template_result['templates']:
+        mapped_templates = list(map(lambda x: json.loads(x), parsed_template_result['templates']))
+        parsed_response['templates'] = mapped_templates
+
+    lines = parsed_template_result['stripped'].splitlines()
+    for idx, x in enumerate(lines):
+        if x.find('validated,') > -1:
+            result = _parse_valided_line(x)
 
     return parsed_response
+
+def _parse_valided_line(line):
+    x = line.split(',')
+
+    file_name = x[0].split()[0]
+    failures = x[1].split()[0]
+    failures = failures.strip()
+
+    result = {
+        'file': file_name,
+        'failures': int(failures)
+
+    }
+
+    return result
 
 def _parse_template_results(response):
     import regex
@@ -39,7 +61,6 @@ def _parse_template_results(response):
     for json_string in returned_list:
         output = output.replace(json_string, "")
 
-    print(output)
     parsed_results = {
         'stripped': output,
         'templates': returned_list
@@ -63,7 +84,6 @@ def _get_container(manifest_file):
     container_name = "cpacontainer"
     try:
         container = client.containers.get(container_name)
-
     except docker.errors.NotFound:
         mount_path = _get_mount_path(manifest_file)
         container = _run_container(container_name, mount_path)
@@ -76,9 +96,10 @@ def _run_container(container_name, mount_path):
     client = docker.from_env()
     home_directory = os.path.expanduser('~')
     absolute_path = f'{home_directory}/.azure'
-    img = 'bobjac/cnab:8.0'
+    img = 'bobjac/cnab:9.0'
     cmd = 'sleep infinity'
     volumes = ['/var/run/docker.sock:/var/run/docker.sock', f'{mount_path}:/cpaMount', f'{absolute_path}:/root/.azure']
+
     container = client.containers.run(img, cmd, detach=True, volumes=volumes, name=container_name)
     return container
 
