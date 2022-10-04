0ba96de3ba1060719aac3c39190d2e4b6828daaa
Bob Jacobs <bob.jacobs@outlook.com>
2022-10-04 01:32:19 -0400

    Bobjac/dev (#24)
    * moved command to az partnercenter marketplace bundle
    
    * updating container image to include new arm-ttk release and adding happy path test data
    
    * resolving issues with the docker call
    
    * moving to _cnab_util.py
    
    * adding requirements.txt file
    
    * moving utils and cleanup
    
    * changing container_image to latest microsoft container.  this will support buildbundle but not verify
    
    * reverting to bobjac/cnab:8.0 until verify command is released in mcr.microsoft.com/container-package-app:latest
    
    * starting refactor to docker api
    
    * fix merge conflict
    
    * Moving from subcommand to docker api
    
    * parsing output from cnab for templates
    
    * adding parsing
    
    * cleaning initial parsing
    
    * initial parser commit
    
    * adding parser fix and cleanup
    
    * initial cleanup
    
    * azdev style cleanup
    
    * refactor with additional extracted methods

--- partnercenter/azext_partnercenter/operations/marketplace_bundle/_cnab_util.py
+++ partnercenter/azext_partnercenter/operations/marketplace_bundle/_cnab_util.py
@@ -9,11 +9,9 @@ from knack.util import CLIError
 def verify(manifest_file):
     container = _get_container(manifest_file)
     result = container.exec_run('cpa verify --directory ./cpaMount')
-
     vr = VerifyResult(result.output.decode("utf-8"))
-    dict = vr.to_list()
+    return vr.to_list()
 
-    return dict
 
 def bundle(manifest_file):
     container = _get_container(manifest_file)
@@ -22,17 +20,19 @@ def bundle(manifest_file):
     result = container.exec_run('cpa buildbundle', workdir='/cpaMount')
     return result
 
+
 def _print_container_result(result):
     output = result.output.decode("utf-8")
     print(output)
+
 
 def _print_result(result):
     error = result.stderr.decode("utf-8")
     if len(error) > 0:
         print(error)
-
     output = result.stdout.decode("utf-8")
     print(output)
+
 
 def _get_container(manifest_file):
     client = docker.from_env()
@@ -44,8 +44,8 @@ def _get_container(manifest_file):
         container = _run_container(container_name, mount_path)
     except:
         raise CLIError('There was an unknow error when trying to find the cpacontainer')
-    finally:
-        return container
+    return container
+
 
 def _run_container(container_name, mount_path):
     client = docker.from_env()
@@ -54,12 +54,13 @@ def _run_container(container_name, mount_path):
     img = 'bobjac/cnab:9.0'
     cmd = 'sleep infinity'
     volumes = ['/var/run/docker.sock:/var/run/docker.sock', f'{mount_path}:/cpaMount', f'{absolute_path}:/root/.azure']
-
     container = client.containers.run(img, cmd, detach=True, volumes=volumes, name=container_name)
     return container
 
+
 def _get_mount_path(manifest_file):
     return os.path.dirname(manifest_file)
+
 
 def _get_acr_name(manifest_file):
     acr_name = ""
@@ -69,7 +70,6 @@ def _get_acr_name(manifest_file):
         index = registry_server.index('.')
         if index > -1:
             acr_name = registry_server[:index]
-
     return acr_name
 
 
@@ -77,18 +77,14 @@ class VerifyResult:
 
     def __init__(self, raw_string):
         self.raw_string = raw_string
-        self.known_validations = ['manifest', 'helm']
 
     def to_list(self):
         parsed_response = []
-
         parsed_json_result = self._parse_json()
-
         if parsed_json_result['templates']:
             mapped_templates = list(map(lambda x: json.loads(x), parsed_json_result['templates']))
             parsed_response['templates'] = mapped_templates
             return parsed_response
-        
         lines = parsed_json_result['stripped'].splitlines()
         lines = list(filter(None, lines))
         for idx, x in enumerate(lines):
@@ -96,89 +92,91 @@ class VerifyResult:
                 result = self._parse_valided_line(x)
                 file = result['file']
                 total_file_failures = result['failures']
-
-                if file.lower() in self.known_validations:
-                    parsed_artifact = {}
-                    parsed_artifact['Artifact Name'] = file
-                    parsed_artifact['Total Failures'] = total_file_failures
-                                
-                    if total_file_failures > 0:
-                        failure_list = self._get_failure_list(lines, idx, file, total_file_failures)
-                        parsed_artifact['Failures'] = failure_list
-                    
-                    parsed_response.append(parsed_artifact)
+                if self._is_known_validation(file):
+                    self._update_known_validation_response(parsed_response, lines, idx, file, total_file_failures)
                 else:
-                    template_files = 0
-                    try:
-                        template_files = int(file)
-                    except ValueError:
-                        template_files = -1
-                    
-                    if template_files > 0:
-                        appended_templates = 0
-                        current_idx = idx + 1
-                        current_file = ''
-                        while appended_templates < template_files:
-                            current_line = lines[current_idx:current_idx+1]
-                            file_info = self._get_template_file_info(lines[current_idx]) 
-                            parsed_artifact = {}
-                            parsed_artifact['Artifact Name'] = file_info['file']
-                            parsed_artifact['Total Failures'] = file_info['failures']
-                    
-                            if file_info['failures'] > 0:
-                                failure_start_idx = current_idx + 1
-                                failure_end_idx = failure_start_idx + file_info['failures']
-                                template_failures = list(map(lambda x: x.strip(), lines[failure_start_idx:failure_end_idx]))
-                                current_idx = current_idx + len(template_failures) + 1
-                                parsed_artifact['Failures'] = template_failures
-                            
-                            parsed_response.append(parsed_artifact)
-                            appended_templates = appended_templates + 1
-                            
+                    self._update_unknown_validation_response(parsed_response, lines, idx, file)
         return parsed_response
 
+    def _update_unknown_validation_response(self, parsed_response, lines, idx, file):
+        template_files = self._get_template_file_cnt(file)
+        if template_files > 0:
+            self._update_template_validation_response(parsed_response, lines, idx, template_files)
+
+    def _update_template_validation_response(self, parsed_response, lines, idx, template_files):
+        appended_templates = 0
+        current_idx = idx + 1
+        while appended_templates < template_files:
+            file_info = self._get_template_file_info(lines[current_idx])
+            parsed_artifact = {}
+            parsed_artifact['Artifact Name'] = file_info['file']
+            parsed_artifact['Total Failures'] = file_info['failures']
+            if file_info['failures'] > 0:
+                template_failures = self._get_template_failures(current_idx + 1, file_info['failures'], lines)
+                parsed_artifact['Failures'] = template_failures
+                current_idx = current_idx + len(template_failures) + 1
+            parsed_response.append(parsed_artifact)
+            appended_templates = appended_templates + 1
+
+    def _update_known_validation_response(self, parsed_response, lines, idx, file, total_file_failures):
+        parsed_artifact = {}
+        parsed_artifact['Artifact Name'] = file
+        parsed_artifact['Total Failures'] = total_file_failures
+        if total_file_failures > 0:
+            failure_list = self._get_failure_list(lines, idx, file, total_file_failures)
+            parsed_artifact['Failures'] = failure_list
+        parsed_response.append(parsed_artifact)
+
+    def _get_parsed_artifact(self, file_info):
+        parsed_artifact = {}
+        parsed_artifact['Artifact Name'] = file_info['file']
+        parsed_artifact['Total Failures'] = file_info['failures']
+        return parsed_artifact
+
+    def _get_template_failures(self, start_idx, failure_cnt, lines):
+        end_idx = start_idx + failure_cnt
+        template_failures = list(map(lambda x: x.strip(), lines[start_idx:end_idx]))
+        return template_failures
+
+    def _get_template_file_cnt(self, file):
+        template_files = 0
+        try:
+            template_files = int(file)
+        except ValueError:
+            template_files = -1
+        return template_files
 
     def _get_template_file_info(self, line):
-        print('inside _get_template_file_info')
         x = line.split(',')
-
         file_part = x[0].split('-')
         file_name = file_part[1].strip()
-        print(f'file_name - {file_name}')
-
         failures = x[1].split()[0]
         failures = failures.strip()
-        print(f'failures - {failures}')
-        
         result = {
             'file': file_name,
             'failures': int(failures)
         }
-
         return result
 
+    def _is_known_validation(self, file):
+        known_validations = ['manifest', 'helm']
+        file = file.lower()
+        return file in known_validations
 
     def _get_failure_list(self, lines, validated_line_idx, file, total_file_failures):
         first_failure_idx = validated_line_idx + 1
         last_failure_idx = (validated_line_idx + total_file_failures) + 1
-        return lines[first_failure_idx:last_failure_idx]        
-
-    def _get_template_failure_list(self, lines, validated_line_idx, file, total_file_failures):
-        failures = []
-        return failures
+        return lines[first_failure_idx:last_failure_idx]
 
     def _parse_valided_line(self, line):
         x = line.split(',')
-
         file_name = x[0].split()[0]
         failures = x[1].split()[0]
         failures = failures.strip()
-
         result = {
             'file': file_name,
             'failures': int(failures)
         }
-
         return result
 
     def _parse_json(self):
@@ -186,10 +184,8 @@ class VerifyResult:
         output = self.raw_string
         pattern = regex.compile(r'\{(?:[^{}]|(?R))*\}')
         returned_list = pattern.findall(output)
-
         for json_string in returned_list:
             output = output.replace(json_string, "")
-
         parsed_results = {
             'stripped': output,
             'templates': returned_list
