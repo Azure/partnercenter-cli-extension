44382e97ae454679b305cc88a32a3c354358ebf6
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-10-17 13:10:21 -0400

    adding params

--- partnercenter/azext_partnercenter/operations/marketplace_offer_plan/_client.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_plan/_client.py
@@ -1,4 +1,4 @@
-from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.apis import ProductClient
+from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.apis import (ProductClient, BranchesClient)
 from partnercenter.azext_partnercenter._client_factory import get_api_client
 import json
 
@@ -6,11 +6,11 @@ class PlanClient():
 
     def __init__(self, cli_ctx, *_):
         self._api_client = get_api_client(cli_ctx, *_)
-        self._client = ProductClient(self._api_client)
-    
+        self._product_client = ProductClient(self._api_client)
+        self._branch_client = BranchesClient(self._api_client)
 
     def list(self):
-        results = self._client.products_get(self._api_client.configuration.access_token)
+        results = self._product_client.products_get(self._api_client.configuration.access_token)
         return list(map(self._to_dict, results.value))
 
 
