4e127f83fac9570a081e587d8f55e1d48e413120
Bob Jacobs <bob.jacobs@outlook.com>
2022-10-18 15:08:16 -0400

    adding create command for offers

--- partnercenter/azext_partnercenter/operations/marketplace_offer/commands.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer/commands.py
@@ -25,7 +25,7 @@ def load_command_table(commands_loader, _):
     custom_command_type = CliCommandType(operations_tmpl='azext_partnercenter.operations.marketplace_offer.custom#{}', client_factory=cf_list)
 
     with commands_loader.command_group('partnercenter marketplace offer', custom_command_type=custom_command_type, is_preview=True) as g:
-        # g.custom_command('create', 'create_offer', supports_no_wait=True, table_transformer=None)
+        g.custom_command('create', 'create_offer', supports_no_wait=True, table_transformer=None)
         # g.custom_command('delete', 'delete_offer', confirmation=True, supports_no_wait=True)
         # g.custom_show_command('show', 'get_offer', table_transformer=None)
         g.custom_command('list', 'list_offer', table_transformer=None)
