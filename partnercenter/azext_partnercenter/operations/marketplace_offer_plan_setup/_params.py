966e0522e2948a7dd415070cd2ce2abe26220b2e
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-10-19 15:30:54 -0400

    scaffolding for offer plan [setup, listing] subgroups (#31)

---
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_plan_setup/_params.py
@@ -0,0 +1,14 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+# pylint: disable=line-too-long
+
+from azure.cli.core.commands.parameters import (get_enum_type,
+                                                get_three_state_flag)
+
+
+def load_arguments(commands_loader, _):
+    with commands_loader.argument_context('partnercenter marketplace offer plan setup') as c:
+        c.argument('offer_resource_id', options_list=['--offer-resource-id'], help='The Offer Resource ID of the Offer for the Plan in the partner center.')
+        c.argument('offer_id', options_list=['--offer-id'], help='The offer ID of the Offer for the Plan.')
