dfac73b32cab0d99eeadbc5c4dfbba6ac0914467
Bob Jacobs <bob.jacobs@outlook.com>
2022-10-19 11:23:11 -0400

    Bobjac/dev (#28)
    * moved command to az partnercenter marketplace bundle
    
    * updating container image to include new arm-ttk release and adding happy path test data
    
    * resolving issues with the docker call
    
    * moving to _cnab_util.py
    
    * adding requirements.txt file
    
    * moving utils and cleanup
    
    * changing container_image to latest microsoft container.  this will support buildbundle but not verify
    
    * reverting to bobjac/cnab:8.0 until verify command is released in mcr.microsoft.com/container-package-app:latest
    
    * starting refactor to docker api
    
    * fix merge conflict
    
    * Moving from subcommand to docker api
    
    * parsing output from cnab for templates
    
    * adding parsing
    
    * cleaning initial parsing
    
    * initial parser commit
    
    * adding parser fix and cleanup
    
    * initial cleanup
    
    * azdev style cleanup
    
    * refactor with additional extracted methods
    
    * comment out latest cnab image until publication
    
    * removing redundant code
    
    * azdev style cleanup
    
    * ensure no index out of range when parsing bundle with no errors
    
    * adding create command for offers
    
    * adding to_dict() to result

--- partnercenter/azext_partnercenter/operations/marketplace_offer/_client.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer/_client.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.apis import ListingClient
 from partnercenter.azext_partnercenter.clients import get_api_client
 import json
@@ -12,6 +13,19 @@ class ListClient():
     def list(self, product_id, instance_id):
         results = self._client.products_product_id_listings_get_by_instance_id_instance_i_dinstance_id_get(product_id, instance_id, self._api_client.configuration.access_token)
         return list(map(self._to_dict, results.value))
+
+    def create(self, offer_id, offer_alias, resource_type):
+        from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.apis import ProductClient
+        from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.models import MicrosoftIngestionApiModelsProductsAzureProduct
+        from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.models import MicrosoftIngestionApiModelsCommonTypeValuePair
+        
+        key_val = MicrosoftIngestionApiModelsCommonTypeValuePair(type='AzureOfferID', value=offer_alias)
+        external_ids = [key_val]
+        prod = MicrosoftIngestionApiModelsProductsAzureProduct(resource_type=resource_type, name=offer_id, id=offer_id, external_ids=external_ids,IsModularPublishing=True)
+        
+        product_client = ProductClient(self._api_client)
+        result = product_client.products_post(self._api_client.configuration.access_token, microsoft_ingestion_api_models_products_azure_product=prod, _content_type="application/json")
+        return result.to_dict()
 
 
     def _to_dict(self, item):
--- partnercenter/azext_partnercenter/operations/marketplace_offer/_params.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer/_params.py
@@ -8,9 +8,13 @@ from azure.cli.core.commands.parameters import (get_enum_type, get_three_state_f
 
 
 def load_arguments(commands_loader, _):
-    pass
-    # with commands_loader.argument_context('partnercenter marketplace offer') as c:
-    #     c.argument('arg', options_list=['--arg', '-a'], help='The argument help')
+    # pass
+
     with commands_loader.argument_context('partnercenter marketplace offer list') as c:
         c.argument('product_id', options_list=['--product_id', '-p'], help='The ID of product')
         c.argument('instance_id', options_list=['--instance_id', '-i'], help='The Resource instance ID')
+
+    with commands_loader.argument_context('partnercenter marketplace offer create') as d:
+        d.argument('offer_id', options_list=['--offer-id', '-id'], help='Use only lowercase, alphanumeric characters, dashes or underscores. ID cannot be modified after selecting Create.')
+        d.argument('offer_alias', options_list=['--offer-alias', '-a'], help='This name won\'t be used in the marketplace listing and is solely for reference within Partner Center.')
+        d.argument('resource_type', options_list=['--resource-type', '-t'], help='The type of offer to create.')
