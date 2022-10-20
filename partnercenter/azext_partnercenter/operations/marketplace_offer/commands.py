64204fd215278e48a0068fd88d255eb950fb791a
Kevin M. Gates <kevinmgates@users.noreply.github.com>
2022-10-20 12:49:17 -0500

    Added list and delete for offers

--- partnercenter/azext_partnercenter/operations/marketplace_offer/commands.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer/commands.py
@@ -19,14 +19,14 @@
 #         #g.custom_command('bundle', 'bundle_offer', supports_no_wait=True, table_transformer=None)
 
 from azure.cli.core.commands import CliCommandType
-from partnercenter.azext_partnercenter._client_factory import cf_list
+from partnercenter.azext_partnercenter._client_factory import cf_offers
 
 def load_command_table(commands_loader, _):
-    custom_command_type = CliCommandType(operations_tmpl='azext_partnercenter.operations.marketplace_offer.custom#{}', client_factory=cf_list)
+    custom_command_type = CliCommandType(operations_tmpl='azext_partnercenter.operations.marketplace_offer.custom#{}', client_factory=cf_offers)
 
     with commands_loader.command_group('partnercenter marketplace offer', custom_command_type=custom_command_type, is_preview=True) as g:
         g.custom_command('create', 'create_offer', supports_no_wait=True, table_transformer=None)
-        # g.custom_command('delete', 'delete_offer', confirmation=True, supports_no_wait=True)
+        g.custom_command('delete', 'delete_offer', confirmation=True, supports_no_wait=True, table_transformer=None)
         # g.custom_show_command('show', 'get_offer', table_transformer=None)
-        g.custom_command('list', 'list_offer', table_transformer=None)
+        g.custom_command('list', 'list_offers', table_transformer=None)
         #g.custom_command('bundle', 'bundle_offer', supports_no_wait=True, table_transformer=None)
