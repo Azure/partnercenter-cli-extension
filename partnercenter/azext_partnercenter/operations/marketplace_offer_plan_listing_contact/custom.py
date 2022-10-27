88611992f35ce79de1ed5696f4dd8c4c1ded31a2
Bob Jacobs <bob.jacobs@outlook.com>
2022-10-27 10:31:20 -0400

    Bobjac/dev (#42)
    * moved command to az partnercenter marketplace bundle
    
    * updating container image to include new arm-ttk release and adding happy path test data
    
    * resolving issues with the docker call
    
    * moving to _cnab_util.py
    
    * adding requirements.txt file
    
    * moving utils and cleanup
    
    * changing container_image to latest microsoft container.  this will support buildbundle but not verify
    
    * reverting to bobjac/cnab:8.0 until verify command is released in mcr.microsoft.com/container-package-app:latest
    
    * starting refactor to docker api
    
    * fix merge conflict
    
    * Moving from subcommand to docker api
    
    * parsing output from cnab for templates
    
    * adding parsing
    
    * cleaning initial parsing
    
    * initial parser commit
    
    * adding parser fix and cleanup
    
    * initial cleanup
    
    * azdev style cleanup
    
    * refactor with additional extracted methods
    
    * comment out latest cnab image until publication
    
    * removing redundant code
    
    * azdev style cleanup
    
    * ensure no index out of range when parsing bundle with no errors
    
    * adding create command for offers
    
    * adding to_dict() to result
    
    * adding post to variant
    
    * adding create plan
    
    * adding PlanList update
    
    * fixing issues in update of plan listing
    
    * moving update implementation to generic update pattern
    
    * adding subcommands for plan listing contacts
    
    * fixing merge issues and cleanup
    
    * stashed commit
    
    * adding subcommands to add contacts
    
    * refactor and cleanup
    
    * cleanup

--- partnercenter/azext_partnercenter/operations/marketplace_offer_plan_listing_contact/custom.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_plan_listing_contact/custom.py
@@ -13,33 +13,7 @@ from partnercenter.azext_partnercenter.models.listing_contact import ListingCont
 # pylint: disable=too-many-locals
 
 def marketplace_offer_plan_listing_contact_update_get(cmd, client, plan_external_id, product_external_id, type=None, email=None, name=None, phone=None, uri=None):
-    plan_listing = client.get_plan_listing(product_external_id, plan_external_id)
-    print(f'plan_listing - {plan_listing}')
-    #plan_listing = PlanListing()
-    #plan_listing.external_id = plan_external_id
-
-    # plan_listing.id = result.id
-   # plan_listing.language_code = result.language_code
-    
-    #plan_listing.uris = []
-    #plan_listing.odata_etag = result.odata_etag
-    #plan_listing.description = result.description
-    #plan_listing.short_description = result.short_description
-
-    #contacts = []
-    #for c in result.listing_contacts:
-    #    listing_contact = ListingContact()
-    #    listing_contact.email = c.email
-    #    listing_contact.phone = c.phone
-    #    listing_contact.name = c.name
-    #    listing_contact.uri = c.uri
-    #    listing_contact.type = c.type
-    #    contacts.append(listing_contact)
-
-    #plan_listing.contacts = contacts
-
-    #plan_listing.contacts = list(map())
-    
+    plan_listing = client.get_plan_listing(product_external_id, plan_external_id)    
     return plan_listing
 
 def marketplace_offer_plan_listing_contact_update_set(cmd, client, plan_external_id, product_external_id, type=None, email=None, name=None, phone=None, uri=None, parameters=None):
