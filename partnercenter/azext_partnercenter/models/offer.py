3af440103ef8dd853453519d30ec3cb2d6aa83ec
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-11-07 12:25:51 -0500

    offer type enum

--- partnercenter/azext_partnercenter/models/offer.py
+++ partnercenter/azext_partnercenter/models/offer.py
@@ -2,7 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
+from enum import Enum
+from azure.core import CaseInsensitiveEnumMeta
 from msrest.serialization import Model
 
 
@@ -17,3 +18,20 @@ class Offer(Model):
         self.id = kwargs.get('id', None)
         self.name = kwargs.get('name', None)
         self._resource = kwargs.get('resource', None)
+
+
+class OfferType(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """The offer types"""
+
+    SOFTWAREASASERVICE = "SoftwareAsAService"
+    AZUREAPPLICATION = "AzureApplication"
+    AZURETHIRDPARTYVIRTUALMACHINE = "AzureThirdPartyVirtualMachine"
+    AZURECONTAINER = "AzureContainer"
+    AZURECONSULTINGSERVICE = "AzureConsultingService"
+    AZUREDYNAMICS365FORCUSTOMERENGAGEMENT = "AzureDynamics365ForCustomerEngagement"
+    AZUREDYNAMICS365FOROPERATIONS = "AzureDynamics365ForOperations"
+    AZUREDYNAMICS365BUSINESSCENTRAL = "AzureDynamics365BusinessCentral"
+    AZUREIOTEDGEMODULE = "AzureIoTEdgeModule"
+    AZUREMANAGEDSERVICE = "AzureManagedService"
+    AZUREPOWERBIAPP = "AzurePowerBIApp"
+    AZUREPOWERBIVISUAL = "AzurePowerBIVisual"
