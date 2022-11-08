d70ca3c42d34800119e36e86b025342367aba2b6
Bob Jacobs <bob.jacobs@outlook.com>
2022-11-07 21:19:50 -0500

    adding create command for offer

--- partnercenter/azext_partnercenter/clients/offer_client.py
+++ partnercenter/azext_partnercenter/clients/offer_client.py
@@ -10,6 +10,8 @@ from partnercenter.azext_partnercenter.models import (ListingContact,
 from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.apis import (
     BranchesClient, ListingClient, ProductClient)
 
+from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.model.microsoft_ingestion_api_models_products_azure_product import MicrosoftIngestionApiModelsProductsAzureProduct
+from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.model.microsoft_ingestion_api_models_common_type_value_pair import MicrosoftIngestionApiModelsCommonTypeValuePair
 from ._util import get_api_client
 
 
@@ -29,6 +31,18 @@ class OfferClient():
                 name=product.name,
                 resource=Resource(id=product.id, type=product.resource_type)
             ), results))
+
+    def create(self, offer_id, offer_alias, resource_type):
+        external_id = MicrosoftIngestionApiModelsCommonTypeValuePair(type='AzureOfferID', value=offer_alias)
+        external_ids = [external_id]
+        authorization = self._get_access_token()
+        product = MicrosoftIngestionApiModelsProductsAzureProduct(external_ids=external_ids, name=offer_id, resource_type=resource_type, id=offer_id, is_modular_publishing=True)
+        product = self._product_client.products_post(authorization=authorization, microsoft_ingestion_api_models_products_azure_product=product)
+        return Offer(
+            id=(next((x for x in product.externalIDs if x['type'] == "AzureOfferId"), None))['value'],
+            name=product.name,
+            resource=Resource(id=product.id, type=product.resource_type)
+        )
 
     def get(self, offer_external_id):
         filter_expr = self._get_filter_by_offer_id_expression(offer_external_id)
