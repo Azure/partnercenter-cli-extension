ec8f0b630996cab993072e2cead5ee9b76d70708
Bob Jacobs <bob.jacobs@outlook.com>
2022-11-09 00:28:39 -0500

    adding offer listing commands

--- partnercenter/azext_partnercenter/operations/marketplace_offer_listing/commands.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_listing/commands.py
@@ -6,12 +6,17 @@
 
 from azure.cli.core.commands import CliCommandType
 
-from partnercenter.azext_partnercenter._client_factory import cf_offers
+from partnercenter.azext_partnercenter._client_factory import cf_listing
 
 
 def load_command_table(commands_loader, _):
-    custom_command_type = CliCommandType(operations_tmpl='azext_partnercenter.operations.marketplace_offer_listing.custom#{}', client_factory=cf_offers)
+    custom_command_type = CliCommandType(operations_tmpl='azext_partnercenter.operations.marketplace_offer_listing.custom#{}', client_factory=cf_listing)
 
     with commands_loader.command_group('partnercenter marketplace offer listing', custom_command_type=custom_command_type, is_preview=True) as g:
         g.custom_show_command('show', 'get_listing', table_transformer=None)
-        g.custom_command('update', 'update_listing', supports_no_wait=True, table_transformer=None)
+        g.generic_update_command('update',
+                                    getter_name='marketplace_offer_listing_update_get',
+                                    setter_name='marketplace_offer_listing_update_set',
+                                    custom_func_name='marketplace_offer_listing_update_custom',
+                                    custom_func_type=custom_command_type,
+                                    client_factory=cf_listing)
--- partnercenter/azext_partnercenter/operations/marketplace_offer_listing/custom.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_listing/custom.py
@@ -6,13 +6,33 @@
 from azure.cli.core.util import sdk_no_wait
 from knack.util import CLIError
 from azure.cli.core.azclierror import (RequiredArgumentMissingError, ResourceNotFoundError)
+from partnercenter.azext_partnercenter.models.listing import Listing
 
 
 def get_listing(client, offer_id):
     return client.get_listing(offer_id)
 
 
-def update_listing(cmd, instance, arg):
-    # TODO: Implement partnercenter marketplace offer update
+def marketplace_offer_listing_update_get(client, product_external_id, summary, short_description, description):
+    listing = client.get_listing(product_external_id)    
+    return listing
+
+def marketplace_offer_listing_update_custom(instance, product_external_id, summary, short_description, description):
+    instance.summary = summary
+    instance.short_description = short_description
+    instance.description = description
     return instance
 
+def marketplace_offer_listing_update_set(cmd, client, product_external_id, summary, short_description, description, parameters=None):
+    listing = Listing()
+    listing.id = parameters.id
+    listing.title = parameters.title
+    listing.summary = parameters.summary
+    listing.description = parameters.description
+    listing.short_description = parameters.short_description
+    listing.language_code = parameters.language_code
+    listing.odata_etag = parameters.odata_etag
+    listing.contacts = parameters.contacts
+    listing.uris = parameters.uris
+    result = client.create_or_update(product_external_id, listing)
+    return result
