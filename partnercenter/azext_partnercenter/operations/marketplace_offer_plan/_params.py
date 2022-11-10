ff6bd3bace0f611761c35848c0470cd4f4ccaef7
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-11-10 08:31:39 -0500

    offer plan id params

--- partnercenter/azext_partnercenter/operations/marketplace_offer_plan/_params.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_plan/_params.py
@@ -9,18 +9,15 @@ from azure.cli.core.commands.parameters import (get_enum_type,
 
 
 def load_arguments(commands_loader, _):
-    with commands_loader.argument_context('partnercenter marketplace offer plan list') as c:
-        c.argument('offer_id', options_list=['--offer-id'], help='The offer id in the partner center.')
-
-    with commands_loader.argument_context('partnercenter marketplace offer plan create') as c:
-        c.argument('product_external_id', options_list=['--product-external-id'], help='The system ID of the offer in the partner center.')
-        c.argument('external_id', options_list=['--external-id'], help='The external ID')
-        c.argument('friendly_name', options_list=['--friendly-name'], help='The friendly name')
+    with commands_loader.argument_context('partnercenter marketplace offer plan') as c:
+        c.argument('offer_id', options_list=['--offer-id'], help='The Offer ID.')
+    
+    with commands_loader.argument_context('partnercenter marketplace offer plan update') as c:
+        c.argument('plan_id', options_list=['--plan-id', '--id'], help='The Plan ID')
 
     with commands_loader.argument_context('partnercenter marketplace offer plan delete') as c:
-        c.argument('offer_id', options_list=['--offer-id'], help='The Offer ID')
-        c.argument('plan_id', options_list=['--plan-id'], help='The Plan ID')
+        c.argument('plan_id', options_list=['--plan-id', '--id'], help='The Plan ID')
 
-    with commands_loader.argument_context('partnercenter marketplace offer plan show') as c:
-        c.argument('plan_resource_id', options_list=['--plan-resource-id'], help='The Offer Resource ID of the Offer for the Plan in the partner center.')
-        c.argument('plan_id', options_list=['--plan-id', '--id'], help='The Plan ID.')
+    with commands_loader.argument_context('partnercenter marketplace offer plan create') as c:
+        c.argument('plan_id', options_list=['--plan-id', '--id'], help='The Plan ID')
+        c.argument('friendly_name', options_list=['--friendly-name'], help='The friendly name')
