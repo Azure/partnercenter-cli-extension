a183358e62e902bc26b2cb702aa8c2c6026d7733
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-11-10 08:36:13 -0500

    offer plan listing ids

--- partnercenter/azext_partnercenter/operations/marketplace_offer_plan_listing/custom.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_plan_listing/custom.py
@@ -15,11 +15,11 @@ def list_listing(cmd, client, offer_id=None, offer_resource_id=None):
    pass
 
 
-def marketplace_offer_plan_listing_update_get(cmd, client, external_id, product_external_id, description=None, short_description=None, language_code=None):
-    plan_listing = client.get_plan_listing(product_external_id, external_id)
+def marketplace_offer_plan_listing_update_get(client, offer_id, plan_id, description=None, short_description=None, language_code=None):
+    plan_listing = client.get_plan_listing(offer_id, plan_id)
     return plan_listing
     
-def marketplace_offer_plan_listing_update_set(cmd, client, external_id, product_external_id, description=None, short_description=None, language_code=None, parameters=None):
+def marketplace_offer_plan_listing_update_set(client, offer_id, plan_id, description=None, short_description=None, language_code=None, parameters=None):
     listing = Listing()
     listing.id = parameters.id
     listing.summary = parameters.summary
@@ -30,16 +30,16 @@ def marketplace_offer_plan_listing_update_set(cmd, client, external_id, product_
     listing.odata_etag = parameters.odata_etag
     listing.contacts = parameters.contacts
     listing.uris = parameters.uris
-    result = client.create_or_update(product_external_id, listing, external_id)
+    result = client.create_or_update(offer_id, listing, plan_id)
     return result
 
-def marketplace_offer_plan_listing_update_custom(instance, external_id, product_external_id, description=None, short_description=None, language_code=None):
+def marketplace_offer_plan_listing_update_custom(instance, description=None, short_description=None, language_code=None):
     instance.short_description = short_description
     instance.description = description
     instance.laguage_code = language_code
     return instance
 
-def get_listing(cmd, client, offer_id=None, offer_resource_id=None, plan_id=None, plan_resource_id=None):
+def get_listing(cmd, client, offer_id=None, plan_id=None):
     pass
 
 def add_listing(cmd, arg):
