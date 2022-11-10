26390f8cb9939ea18cbac53f1e7e28377a87b58a
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-11-10 12:25:56 -0500

    plan technical configuration show

--- partnercenter/azext_partnercenter/clients/plan_technicalconfiguration_client.py
+++ partnercenter/azext_partnercenter/clients/plan_technicalconfiguration_client.py
@@ -3,16 +3,20 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-
-from azext_partnercenter.clients._base_client import BaseClient
 from azext_partnercenter.clients import OfferClient, PlanClient
-from ._util import object_to_dict
-from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.apis import (
-    PackageClient as SdkPackageClient, ProductClient as SdkProductClient)
+from azext_partnercenter.clients._base_client import BaseClient
+
+from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.exceptions import \
+    NotFoundException
+
+from ._util import (get_combined_paged_results, object_to_dict)
+from knack.util import CLIError
 
 class PlanTechnicalConfigurationClient(BaseClient):
+    PACKAGE_MODULE = "Package"
+
     def __init__(self, cli_ctx, *_):
-        BaseClient.__init__(self, cli_ctx, *_)
+        super(PlanTechnicalConfigurationClient, self).__init__(cli_ctx, *_)
 
         self._offer_client = OfferClient(cli_ctx, *_)
         self._plan_client = PlanClient(cli_ctx, *_)
@@ -24,10 +28,31 @@ class PlanTechnicalConfigurationClient(BaseClient):
             # TODO: determine if throwing CLIError is better here
             return None
         
-        product_id = product.id
-        variants = None
-        return {}
+        variants = get_combined_paged_results(lambda : self._sdk.variant_client.products_product_id_variants_get(product.id, self._get_access_token()))
+        variant = next((v for v in variants if v['externalID'] == plan_external_id), None)
 
+        if variant is None:
+            raise CLIError(f'Plan not found for "{plan_external_id}"')
+ 
+        branches = self._sdk.branches_client.products_product_id_branches_get_by_module_modulemodule_get(
+                                             product.id, self.PACKAGE_MODULE, self._get_access_token())
+        if len(branches.value) == 0:
+            return None
+        
+        variant_package_branch = next((b for b in branches.value if hasattr(b, 'variant_id') and b.variant_id == variant['id']), None)
 
-    def delete(self, offer_external_id):
-       pass
+        if variant_package_branch is None:
+            raise CLIError(f'Technical Configuration not found for Plan ID "{plan_external_id}"')
+        
+        try:
+            self._sdk.package_client.products_product_id_packages_get_endpoint.settings['endpoint_path'] += f"/getByInstanceID(instanceID={plan_external_id})"
+            # print(object_to_dict(self._sdk.package_client.products_product_id_packages_get_endpoint.settings))
+
+            package = self._sdk.package_client.products_product_id_packages_get(product.id, self._get_access_token())
+            print(object_to_dict(package))
+
+            return packages
+        except NotFoundException:
+            pass
+
+        return None
