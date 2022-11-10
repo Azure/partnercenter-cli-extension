b3f2887bea081929804d3eeabfc284d131eba194
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-11-10 08:20:41 -0500

    offer params clean up

--- partnercenter/azext_partnercenter/operations/marketplace_offer_listing_media/_params.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_listing_media/_params.py
@@ -10,8 +10,7 @@ from azure.cli.core.commands.parameters import (get_enum_type,
 
 def load_arguments(commands_loader, _):
     with commands_loader.argument_context('partnercenter marketplace offer listing media') as c:
-        #c.argument('plan_external_id', options_list=['--offer-id'], help='The offer id.')
-        c.argument('product_external_id', options_list=['--offer-id'], help='The product external id.')
+        c.argument('offer_id', options_list=['--offer-id'], help='The Offer ID.')
         c.argument('type', options_list=['--type'], help='The type of the uri.')
         c.argument('file', options_list=['--file'], help='The type of the uri.')
 
