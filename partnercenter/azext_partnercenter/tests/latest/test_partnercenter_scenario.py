079d471c9c4471bf01355ec7faafbccd123d31e6
Bob Jacobs <bob.jacobs@outlook.com>
2022-11-11 12:58:18 -0500

    adding uri validation to scenario test

--- partnercenter/azext_partnercenter/tests/latest/test_partnercenter_scenario.py
+++ partnercenter/azext_partnercenter/tests/latest/test_partnercenter_scenario.py
@@ -21,6 +21,10 @@ class PartnercenterScenarioTest(ScenarioTest):
         summary = self.create_random_name('os', 15)
         short_description = self.create_random_name('sd', 10)
         description = self.create_random_name('d', 20)
+        uri = 'http://www.microsoft.com'
+        uri_display_text = self.create_random_name('d', 20)
+        uri_type = ''
+        uri_sub_type = ''
 
         self.kwargs.update({
             'offer_id': offer_id,
@@ -28,7 +32,11 @@ class PartnercenterScenarioTest(ScenarioTest):
             'offer_type': offer_type,
             'summary': summary,
             'short_description': short_description,
-            'description': description
+            'description': description,
+            'uri': uri,
+            'uri_display_text': uri_display_text,
+            'uri_type': uri_type,
+            'uri_sub_type': uri_sub_type
         })
 
         self.cmd('partnercenter marketplace offer create --offer-id {offer_id} --offer-alias {offer_alias} --offer-type {offer_type}',
@@ -40,11 +48,22 @@ class PartnercenterScenarioTest(ScenarioTest):
                         self.check('shortDescription', '{short_description}'),
                         self.check('summary', '{summary}')])
 
-        self.cmd('partnercenter marketplace offer listing show --offer-id {offer_id}',
+        self.cmd('az partnercenter marketplace offer listing show --offer-id {offer_id}',
                 checks=[self.check('description', '{description}'),
                         self.check('shortDescription', '{short_description}'),
                         self.check('summary', '{summary}'),
                         self.check('contacts', []),
                         self.check('uris', [])])
+
+        self.cmd('partnercenter marketplace offer listing uri add --offer-id {offer_id} --type {uri_type} --subtype {uri_sub_type} --display-text {uri_display_text} --uri {uri}',
+                checks=[self.check('description', '{description}'),
+                        self.check('shortDescription', '{short_description}'),
+                        self.check('summary', '{summary}'),
+                        self.check('contacts', []),
+                        self.check('uris[0].type', {uri_type}),
+                        self.check('uris[0].subtype', {uri_sub_type}),
+                        self.check('uris[0].displayText', {uri_display_text}),
+                        self.check('uris[0].uri', {uri})])
+        
 
         self.cmd('partnercenter marketplace offer delete --offer-id {offer_id} -y')
