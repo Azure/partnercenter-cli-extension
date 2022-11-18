cb5b7a4fa2ac7c443f3469049fde1dc49153c265
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-11-18 09:46:44 -0500

    implementing internal client factory

--- partnercenter/azext_partnercenter/clients/_product_ingestion_api_client.py
+++ partnercenter/azext_partnercenter/clients/_product_ingestion_api_client.py
@@ -9,28 +9,39 @@ from ._sdk_client_provider import SdkClientProvider
 
 
 class ProductIngestionApiClientConfiguration:
+    """Configuration for the Product Ingestion API Client
+    
+        Attributes:
+            server_resource     The server resource path that will be used to get the correct access token
+    """
+
+    server_resource = "https://graph.microsoft.com"
+
     def __init__(self, access_token=None):
         self._base_path = "https://graph.microsoft.com/rp/product-ingestion"
         self.access_token = access_token
-        self._endpoint_versions = {
+        self.endpoint_versions = {
             'resource-tree': '2022-03-01-preview3'
         }
 
 
     def get_version(self, endpoint_name):
-        return self._endpoint_versions.get(endpoint_name)
+        return self.endpoint_versions.get(endpoint_name)
 
 
 class ProductIngestionApiClient:
     """The Product Ingestion API client"""
-    def __init__(self, access_token):
+    def __init__(self, access_token=None):
         self.configuration = ProductIngestionApiClientConfiguration(access_token=access_token)
-
+        self._default_headers = { 'Accept': 'application/json' }
     
     def get_resource_tree(self, offer_durable_id):
         path = f'resource-tree/product/{offer_durable_id}'
         response = self.__call_api('resource-tree', path)
-
+        return response
+        
+    def set_default_header(self, key, value):
+        self._default_headers[key] = value
 
     def __call_api(self, endpoint_name, path, params=None):
         url = f'{self.configuration._base_path}/{path}'
@@ -41,10 +52,7 @@ class ProductIngestionApiClient:
 
 
     def __get_request_headers(self):
-        return { 
-            'Accept': 'application/json', 
-            'Authorization': f'Bearer {self._graph_api_client.configuration.access_token}'
-        }
+        return self._default_headers
 
     
     def __merge_params(self, a, b):
