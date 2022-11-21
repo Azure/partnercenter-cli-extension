6d41956bac0da85ddb825e76c11fcfa1caa65efb
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-11-21 14:54:27 -0500

    removing offer plan setup

--- partnercenter/azext_partnercenter/operations/marketplace_offer_setup/_params.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_setup/_params.py
@@ -10,7 +10,7 @@ from azure.cli.core.commands.parameters import (get_enum_type,
 
 def load_arguments(commands_loader, _):
     with commands_loader.argument_context('partnercenter marketplace offer setup') as c:
-        c.argument('offer_id', options_list=['--offer-id'], help='The offer ID of the Offer.')
+        c.argument('offer_id', options_list=['--offer-id'], help='The Offer ID.')
         c.argument('reseller_enabled', options_list=['--enable-resell'], help='Boolen value indicating if reseller is enabled.')
         c.argument('test_drive_enabled', options_list=['--enable-test-drive'], help='Boolen value indicating if test drive is enabled.')
         c.argument('selling_option', options_list=['--selling-option'], help='The selling option.')
