a19b3a72fd06c9659880b4da9ea2fba28a7b2c96
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-11-28 16:02:00 -0500

    offer_client BaseClient impl

--- partnercenter/azext_partnercenter/clients/offer_client.py
+++ partnercenter/azext_partnercenter/clients/offer_client.py
@@ -9,38 +9,29 @@ from partnercenter.azext_partnercenter._util import get_combined_paged_results
 from partnercenter.azext_partnercenter.models import (ListingContact,
                                                       ListingUri, Offer,
                                                       Listing, Resource)                                               
-from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.apis import (
-    BranchesClient, ListingClient, ProductClient)
 
-from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.model.microsoft_ingestion_api_models_products_azure_product import MicrosoftIngestionApiModelsProductsAzureProduct
-from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.model.microsoft_ingestion_api_models_common_type_value_pair import MicrosoftIngestionApiModelsCommonTypeValuePair
-from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.model.microsoft_ingestion_api_models_products_azure_product_setup import MicrosoftIngestionApiModelsProductsAzureProductSetup
-from ._client_factory import get_api_client
+from partnercenter.azext_partnercenter.vendored_sdks.v1.partnercenter.models import MicrosoftIngestionApiModelsProductsAzureProduct, MicrosoftIngestionApiModelsCommonTypeValuePair, MicrosoftIngestionApiModelsProductsAzureProductSetup
 
 class OfferClient(BaseClient):
 
     def __init__(self, cli_ctx, *_):
-        BaseClient.__init__(self, cli_ctx, *_)
-
-        self._product_client = ProductClient(self._api_client)
-        self._branches_client = BranchesClient(self._api_client)
-        self._listing_client = ListingClient(self._api_client)
+        super(OfferClient, self).__init__(cli_ctx, *_)
 
     
     def delete(self, offer_external_id):
         filter_expr = self._get_sdk_odata_filter_expression_by_external_offer_id(offer_external_id)
-        products = self._product_client.products_get(self._get_access_token(), filter=filter_expr)
+        products = self._sdk.product_client.products_get(self._get_access_token(), filter=filter_expr)
 
         if (len(products.value) == 0):
             return
         
         product_id = products.value[0].id
-        response = self._product_client.products_product_id_delete(product_id, self._get_access_token())
+        response = self._sdk.product_client.products_product_id_delete(product_id, self._get_access_token())
 
         return response
         
     def list(self):
-        results = get_combined_paged_results(lambda skip_token: self._product_client.products_get(self._get_access_token(), skip_token=skip_token))
+        results = get_combined_paged_results(lambda skip_token: self._sdk.product_client.products_get(self._get_access_token(), skip_token=skip_token))
         return list(map(lambda product : Offer(
                 id=(next((x for x in product.externalIDs if x['type'] == "AzureOfferId"), None))['value'],
                 name=product.name,
@@ -53,7 +44,7 @@ class OfferClient(BaseClient):
         external_ids = [external_id]
         authorization = self._get_access_token()
         product = MicrosoftIngestionApiModelsProductsAzureProduct(external_ids=external_ids, name=offer_alias, resource_type=resource_type, id=offer_external_id, is_modular_publishing=True)
-        product = self._product_client.products_post(authorization=authorization, microsoft_ingestion_api_models_products_azure_product=product)
+        product = self._sdk.product_client.products_post(authorization=authorization, microsoft_ingestion_api_models_products_azure_product=product)
         return Offer(
             id=(next((x for x in product.externalIDs if x['type'] == "AzureOfferId"), None))['value'],
             name=product.name,
@@ -63,7 +54,7 @@ class OfferClient(BaseClient):
 
     def get(self, offer_external_id):
         filter_expr = self._get_sdk_odata_filter_expression_by_external_offer_id(offer_external_id)
-        products = self._product_client.products_get(self._get_access_token(), filter=filter_expr)
+        products = self._sdk.product_client.products_get(self._get_access_token(), filter=filter_expr)
 
         if (len(products.value) == 0):
             return None
@@ -79,11 +70,11 @@ class OfferClient(BaseClient):
 
     def delete(self, offer_external_id):
         offer = self.get(offer_external_id)
-        return self._product_client.products_product_id_delete(offer._resource.durable_id, self._get_access_token())
+        return self._sdk.product_client.products_product_id_delete(offer._resource.durable_id, self._get_access_token())
 
     def get_setup(self, offer_external_id):
         offer = self.get(offer_external_id)
-        result = self._product_client.products_product_id_setup_get(offer._resource.durable_id, self._get_access_token())
+        result = self._sdk.product_client.products_product_id_setup_get(offer._resource.durable_id, self._get_access_token())
         return self._map_setup(result)
 
     def create_setup(self, offer_external_id, test_drive_enabled: bool, reseller_enabled: bool, selling_option, trial_uri):
@@ -98,7 +89,7 @@ class OfferClient(BaseClient):
         channel_state= MicrosoftIngestionApiModelsCommonTypeValuePair(type='Reseller', value=enabled_value)
         channel_states = [channel_state]
         api_product_setup = MicrosoftIngestionApiModelsProductsAzureProductSetup(resource_type=resource_type, enable_test_drive=test_drive_enabled, selling_option=selling_option,channel_states=channel_states)
-        result = self._product_client.products_product_id_setup_post(offer._resource.durable_id, self._get_access_token(), microsoft_ingestion_api_models_products_azure_product_setup=api_product_setup)
+        result = self._sdk.product_client.products_product_id_setup_post(offer._resource.durable_id, self._get_access_token(), microsoft_ingestion_api_models_products_azure_product_setup=api_product_setup)
         return result
 
     def _map_setup(self, api_setup: MicrosoftIngestionApiModelsProductsAzureProductSetup) -> OfferSetup:
@@ -130,7 +121,7 @@ class OfferClient(BaseClient):
         instance_id = current_draft_module.current_draft_instance_id
 
         listings = get_combined_paged_results(lambda : 
-            self._listing_client.products_product_id_listings_get_by_instance_id_instance_i_dinstance_id_get(
+            self._sdk.listing_client.products_product_id_listings_get_by_instance_id_instance_i_dinstance_id_get(
             offer._resource.durable_id, instance_id, self._get_access_token()))
         
         # TODO: there should only be 1 active listing (that we can confirm as of now)
@@ -156,7 +147,7 @@ class OfferClient(BaseClient):
     def _get_sdk_product_by_external_offer_id(self, offer_external_id):
         """Package Internal helper method to get the SDK product object by Offer ID"""
         filter_expr = self._get_sdk_odata_filter_expression_by_external_offer_id(offer_external_id)
-        products = self._product_client.products_get(self._get_access_token(), filter=filter_expr)
+        products = self._sdk.product_client.products_get(self._get_access_token(), filter=filter_expr)
 
         if products is None or len(products.value) == 0:
             return None
