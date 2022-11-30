b0f7a5e810cd877b20cc563934555cd210a92456
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-11-30 13:01:40 -0500

    help system files for each sub group of operations (#110)

--- partnercenter/azext_partnercenter/_help.py
+++ partnercenter/azext_partnercenter/_help.py
@@ -3,36 +3,22 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+# pylint: disable=unused-import
 
-from knack.help_files import helps  # pylint: disable=unused-import
+from .operations.marketplace_bundle._help import helps
+from .operations.marketplace_offer._help import helps
+from .operations.marketplace_offer_listing._help import helps
+from .operations.marketplace_offer_listing_contact._help import helps
+from .operations.marketplace_offer_listing_media._help import helps
+from .operations.marketplace_offer_listing_uri._help import helps
+from .operations.marketplace_offer_plan._help import helps
+from .operations.marketplace_offer_plan_listing._help import helps
+from .operations.marketplace_offer_plan_technicalconfiguration._help import helps
+from .operations.marketplace_offer_setup._help import helps
+from knack.help_files import helps
 
 
 helps['partnercenter marketplace'] = """
     type: group
     short-summary: Commands to manage the Partner Center Marketplace.
 """
-
-helps['partnercenter marketplace offer create'] = """
-    type: command
-    short-summary: Creates a Marketplace Offer.
-"""
-
-helps['partnercenter marketplace offer list'] = """
-    type: command
-    short-summary: Lists all Marketplace Offers.
-"""
-
-# helps['partnercenter delete'] = """
-#     type: command
-#     short-summary: Delete a Partnercenter.
-# """
-
-# helps['partnercenter show'] = """
-#     type: command
-#     short-summary: Show details of a Partnercenter.
-# """
-
-# helps['partnercenter update'] = """
-#     type: command
-#     short-summary: Update a Partnercenter.
-# """
---
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_listing/_help.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from knack.help_files import helps
+
+
+helps['partnercenter marketplace offer listing'] = """
+    type: group
+    short-summary: Manage a Marketplace Offer's listing.
+"""
