e942896806760de6f9c244f26365c8a6f9d0f405
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-12-01 10:25:32 -0500

    removing submission client reference

--- partnercenter/azext_partnercenter/clients/__init__.py
+++ partnercenter/azext_partnercenter/clients/__init__.py
@@ -5,7 +5,6 @@
 
 from .offer_client import OfferClient
 from .plan_client import PlanClient
-from .submission_client import SubmissionClient
 from .plan_technicalconfiguration_client import PlanTechnicalConfigurationClient
 from .offer_listing_client import OfferListingClient
 from .listing_media_client import ListingMediaClient
@@ -13,7 +12,6 @@ from .listing_media_client import ListingMediaClient
 __all__ = [
     'PlanClient',
     'PackageClient',
-    'SubmissionClient',
     'PlanTechnicalConfigurationClient',
     'PlanListingClient',
     'OfferListingClient',
