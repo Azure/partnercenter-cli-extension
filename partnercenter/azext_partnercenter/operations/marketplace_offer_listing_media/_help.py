121defc753c00fb6c3def8fc458cdfc43c1a59b5
Kevin Hillinger <kevin.hillinger@outlook.com>
2022-12-08 09:50:02 -0500

    media help

--- partnercenter/azext_partnercenter/operations/marketplace_offer_listing_media/_help.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_listing_media/_help.py
@@ -13,4 +13,31 @@ def load_help():
         short-summary: Manage a Marketplace Offer listing's media including images, videos, and thumbnails.
     """
 
+
+    helps['partnercenter marketplace offer listing media add'] = """
+        type: command
+        short-summary: Add media for a marketplace offer listing
+        examples:
+        - name: Add media to an offer's listing
+          text: |-
+                az partnercenter marketplace offer listing media add --offer-id MyOfferId --type LargeLogo
+    """
+
+    helps['partnercenter marketplace offer listing media delete'] = """
+        type: command
+        short-summary: Delete media for a marketplace offer listing
+        examples:
+        - name: Delete a media to an offer's listing
+          text: |-
+                az partnercenter marketplace offer listing media delete --offer-id MyOfferId --type LargeLogo
+    """
+
+    helps['partnercenter marketplace offer listing media list'] = """
+        type: command
+        short-summary: List the media for a marketplace offer listing
+        examples:
+        - name: List the media for an offer's listing
+          text: |-
+                az partnercenter marketplace offer listing media list --offer-id MyOfferId
+    """
     
