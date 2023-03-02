c3b55b0771d585aedf0cf5f7102b93cd7c784e79
Daniel Ciborowski <dciborow@microsoft.com>
2023-03-02 19:38:20 +0000

    update to azdev

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,3 +7,14 @@ pip install azdev
 azdev setup -r .
 azdev extension repo add ../partnercenter-cli-extension/
 azdev extension add partnercenter
+
+clientId=$(echo $AZURE_CREDENTIALS | jq -r .clientId)
+clientSecret=$(echo $AZURE_CREDENTIALS | jq -r .clientSecret)
+tenantId=$(echo $AZURE_CREDENTIALS | jq -r .tenantId)
+
+az login \
+    --service-principal \
+    -u $clientId \
+    -p $clientSecret \
+    --tenant $tenantId \
+    --allow-no-subscriptions
