f5fab4ae0bd2de137e75e050eb7b2a98dc0819df
Daniel Ciborowski <dciborow@microsoft.com>
2023-03-06 17:30:57 +0000

    add setup show

--- partnercenter/azext_partnercenter/tests/latest/test_marketplace_application.py
+++ partnercenter/azext_partnercenter/tests/latest/test_marketplace_application.py
@@ -1,11 +1,33 @@
 from azure.cli.testsdk import ScenarioTest
 
+
 class PartnerCenterMarketplaceApplicationScenarioTest(ScenarioTest):
     def test_solution_template_create(self):
+        """
+        OFFER_ID=offertest1
+        PLAN_ID=stPlanTest1
+
+        az partnercenter marketplace offer...
+        ... create         --offer-id $OFFER_ID --alias $OFFER_ID --type AzureApplication
+        ... listing show   --offer-id $OFFER_ID
+        ... listing update --offer-id $OFFER_ID --summary "a storage offer" --short-description "a short description" --description @description.html
+        ... setup   show   --ofer-id $OFFER_ID
+        ... setup   update --ofer-id $OFFER_ID --reseller false --sell-through-microsoft false --test-drive false
+
+        az partnercenter marketplace offer plan...
+        ... create         --offer-id $OFFER_ID --plan-id $PLAN_ID --name $PLAN_ID
+        ... listing show   --offer-id $OFFER_ID --plan-id $PLAN_ID
+        ... listing update --offer-id $OFFER_ID --plan-id $PLAN_ID --summary "a storage offer" --description @description.html
+
+        az partnercenter marketplace offer plan technical-configuration...
+        ... show --offer-id $OFFER_ID --plan-id $PLAN_ID
+        """
         self._create_offer()
 
         self._show_offer_listing()
-        # self._update_offer_listing()
+        self._update_offer_listing()
+        self._show_offer_setup()
+        # self._update_offer_setup()
 
         self._create_st_plan()
         # self._update_plan_listing()
@@ -15,12 +37,14 @@ class PartnerCenterMarketplaceApplicationScenarioTest(ScenarioTest):
     def test_managed_app_create(self):
         """
         OFFER_ID=offertest1
-        PLAN_ID=plantest1
+        PLAN_ID=maPlanTest1
 
         az partnercenter marketplace offer...
         ... create         --offer-id $OFFER_ID --alias $OFFER_ID --type AzureApplication
         ... listing show   --offer-id $OFFER_ID
-        ... listing update --offer-id $OFFER_ID --summary "a storage offer" --description @description.html
+        ... listing update --offer-id $OFFER_ID --summary "a storage offer" --short-description "a short description" --description @description.html
+        ... setup   show   --ofer-id $OFFER_ID
+        ... setup   update --ofer-id $OFFER_ID --reseller false --sell-through-microsoft false --test-drive false
 
         az partnercenter marketplace offer plan...
         ... create         --offer-id $OFFER_ID --plan-id $PLAN_ID --name $PLAN_ID --subtype managed-application
@@ -32,7 +56,9 @@ class PartnerCenterMarketplaceApplicationScenarioTest(ScenarioTest):
         """
         self._create_offer()
         self._show_offer_listing()
-        # self._update_offer_listing()
+        self._update_offer_listing()
+        self._show_offer_setup()
+        # self._update_offer_setup()
 
         self._create_ma_plan()
         # self._update_plan_listing()
@@ -53,12 +79,12 @@ class PartnerCenterMarketplaceApplicationScenarioTest(ScenarioTest):
                 "offer_alias": f"{self.offer_id}-alias",
                 "summary": "A storage offer for Unreal Engine Game Developers",
                 "short_description": "Unreal Cloud DDC for Unreal Engine game development.",
-                "description": "@description.html",
+                "description": "Unreal Cloud DDC for Unreal Engine game development.",  # "@description.html",
                 "plan_id": self.plan_id,
                 "plan_name": self.plan_id + "name",
                 "plan_summary": "Unreal Cloud DDC for Unreal Engine game development.",
                 "plan_description": "@description.html",
-            },            
+            },
         )
 
     def _create_offer(self):
@@ -68,13 +94,25 @@ class PartnerCenterMarketplaceApplicationScenarioTest(ScenarioTest):
         )
 
     def _show_offer_listing(self):
-        self.cmd('partnercenter marketplace offer listing show --offer-id {offer_id}')
+        self.cmd("partnercenter marketplace offer listing show --offer-id {offer_id}")
 
     def _update_offer_listing(self):
-        self.cmd("partnercenter marketplace offer listing update --offer-id {offer_id} --summary '{summary}'' --short-description '{short_description}' --description '{description}'",
-            checks=[self.check('description', '{description}'),
-                    self.check('shortDescription', '{short_description}'),
-                    self.check('summary', '{summary}')])
+        self.cmd(
+            "partnercenter marketplace offer listing update --offer-id {offer_id} --summary '{summary}' --short-description '{short_description}' --description '{description}'",
+            checks=[
+                self.check("description", "{description}"),
+                self.check("shortDescription", "{short_description}"),
+                self.check("summary", "{summary}"),
+            ],
+        )
+
+    def _show_offer_setup(self):
+        self.cmd("partnercenter marketplace offer setup show --offer-id {offer_id}")
+
+    def _update_offer_setup(self):
+        self.cmd(
+            "partnercenter marketplace offer setup update --offer-id {offer_id} --reseller false --sell-through-microsoft false --test-drive false"
+        )
 
     def _delete_offer(self):
         self.cmd("partnercenter marketplace offer delete --id {offer_id} --yes")
@@ -83,19 +121,19 @@ class PartnerCenterMarketplaceApplicationScenarioTest(ScenarioTest):
 
     def _create_st_plan(self):
         self.cmd(
-            "partnercenter marketplace offer plan create --offer-id {offer_id} --id {plan_id} -n '{plan_name}'",
+            "partnercenter marketplace offer plan create --offer-id {offer_id} --plan-id {plan_id} --name '{plan_name}'",
             checks=[self.check("id", "{plan_id}"), self.check("name", "{plan_name}")],
         )
 
     def _create_ma_plan(self):
         self.cmd(
-            "partnercenter marketplace offer plan create --offer-id {offer_id} --id {plan_id} -n '{plan_name}' --subtype managed-application",
+            "partnercenter marketplace offer plan create --offer-id {offer_id} --plan-id {plan_id} --name '{plan_name}' --subtype managed-application",
             checks=[self.check("id", "{plan_id}"), self.check("name", "{plan_name}")],
         )
 
     def _update_plan_listing(self):
         self.cmd(
-            "partnercenter marketplace offer plan listing update --offer-id {offer_id} --plan-id {plan_id} -n '{plan_name}' -d '{plan_description}' -s '{plan_summary}'",
+            "partnercenter marketplace offer plan listing update --offer-id {offer_id} --plan-id {plan_id} --description '{plan_description}' --summary '{plan_summary}'",
             checks=[self.check("id", "{plan_id}"), self.check("name", "{plan_name}")],
         )
 
