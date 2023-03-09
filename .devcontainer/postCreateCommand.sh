28d251605a9a616b2d4252d0eace91ee22f15e66
Daniel Ciborowski <dciborow@microsoft.com>
2023-03-09 18:38:44 +0000

    add if/else for login

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+# Create a Codespace secert in your fork to log in to Azure
+# https://github.com/azure/login#configure-deployment-credentials
 
 source /home/codespace/env/bin/activate
 
 azdev setup -r . -e partnercenter -d requirements.txt
 
-clientId=$(echo $AZURE_CREDENTIALS | jq -r .clientId)
-clientSecret=$(echo $AZURE_CREDENTIALS | jq -r .clientSecret)
-tenantId=$(echo $AZURE_CREDENTIALS | jq -r .tenantId)
+if [ -z "$AZURE_CREDENTIALS" ]; then
+    echo "Skipping login. Credentials not found."
+else
 
-az login \
-    --service-principal \
-    -u $clientId \
-    -p $clientSecret \
-    --tenant $tenantId \
-    --allow-no-subscriptions
+    clientId=$(echo $AZURE_CREDENTIALS | jq -r .clientId)
+    clientSecret=$(echo $AZURE_CREDENTIALS | jq -r .clientSecret)
+    tenantId=$(echo $AZURE_CREDENTIALS | jq -r .tenantId)
+
+    az login \
+        --service-principal \
+        -u $clientId \
+        -p $clientSecret \
+        --tenant $tenantId \
+        --allow-no-subscriptions
+fi
