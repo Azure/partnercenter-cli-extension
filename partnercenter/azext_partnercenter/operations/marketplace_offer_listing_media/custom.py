abcdc3823a56404bae047c57a60642e54cfe57bf
Bob Jacobs <bob.jacobs@outlook.com>
2023-06-16 10:05:56 -0400

    convert to enum

--- partnercenter/azext_partnercenter/operations/marketplace_offer_listing_media/custom.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_listing_media/custom.py
@@ -2,23 +2,31 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+from azext_partnercenter.models import MediaType
 
 
-def list_media(client, offer_id, media_type="image"):
-    if media_type == "image":
+def list_media(client, offer_id, media_type=None):
+    if media_type is None:
+        media_type = MediaType.Image
+    elif isinstance(media_type, str):
+        media_type = next((m for m in MediaType if m.value.lower() == media_type.lower()), None)
+
+    if media_type is MediaType.Image:
+        print('getting listing images')
         return client.get_listing_images(offer_id)
 
-    if media_type == "video":
+    if media_type is MediaType.Video:
+        print('getting listing videos')
         return client.get_listing_videos(offer_id)
 
-    return None
+    return []
 
 
-def add_media(client, offer_id, file, media_type="image", streaming_uri=None):
-    if media_type == "image":
+def add_media(client, offer_id, file, media_type=MediaType.Image, streaming_uri=None):
+    if media_type == MediaType.Image:
         return client.add_listing_image(offer_id, media_type, file)
 
-    if media_type == "video":
+    if media_type == MediaType.Video:
         return client.add_listing_video(offer_id, file, streaming_uri)
 
     return None
--- partnercenter/azext_partnercenter/operations/marketplace_offer_listing_media/params.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_listing_media/params.py
@@ -3,10 +3,12 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+from azure.cli.core.commands.parameters import get_enum_type
+from azext_partnercenter.models import (MediaType)
 
 def load_arguments(commands_loader, _):
     with commands_loader.argument_context('partnercenter marketplace offer listing media') as c:
-        c.argument('offer_id', options_list=['--offer-id'], help='The Offer ID.')
-        c.argument('media_type', options_list=['--type'], help='The media type.')
-        c.argument('file', options_list=['--file'], help='The path to the media file.')
+        c.argument('offer_id', options_list=['--offer-id', '--id'], help='The Offer ID.')
+        c.argument('media_type', options_list=['--type', '-t'], arg_type=get_enum_type(MediaType), help='The media type.')
+        c.argument('file', options_list=['--file', '-f'], help='The path to the media file.')
         c.argument('streaming_uri', options_list=['--streaming-uri'], help='The streaming URI.')
