df1a04c8595dbe08132f105ac9647aa78cc0d233
Kevin Hillinger <kevin.hillinger@outlook.com>
2023-06-19 08:54:52 -0400

    fixing linter complaints

--- partnercenter/azext_partnercenter/operations/marketplace_offer_listing_media/custom.py
+++ partnercenter/azext_partnercenter/operations/marketplace_offer_listing_media/custom.py
@@ -12,19 +12,11 @@ def list_media(client, offer_id, media_type):
 
 
 def add_media(client, offer_id, file, media_type, streaming_uri=None):
-    if _is_image_media_type(media_type):
-        return client.add_listing_image(offer_id, media_type, file)
     if media_type == MediaType.Video:
         return client.add_listing_video(offer_id, file, streaming_uri)
+    return client.add_listing_image(offer_id, media_type, file)
 
 
 def delete_media(client, offer_id, media_type):
     result = client.delete_listing_image(offer_id, media_type)
     return result
-
-def _is_image_media_type(media_type):
-    return (media_type == MediaType.Image or
-            media_type == MediaType.LogoLarge or
-            media_type == MediaType.LogoMedium or
-            media_type == MediaType.LogoSmall or
-            media_type == MediaType.LogoWide)
