f31db6606b1db9bbb83ff53fe4982a38635c2270
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-07-17 10:53:13 -0400

    now moving x-$defs into components/schemas

--- tools/jsonschema2openapi/definitions.js
+++ tools/jsonschema2openapi/definitions.js
@@ -10,13 +10,29 @@ function readFile(file) {
 
 const definitionsTemplatePath = "./templates/definitions.json"
 
+const moveDefsToCommponentsSchemas = function(spec, component) {
+    //next we now need to move all remaining x-$defs into the component schemas and remove the key
+    if (component.document["x-$defs"]) {
+        const xdefsKeys = Object.keys(component.document["x-$defs"])
+        xdefsKeys.forEach(key => {
+            if (spec.components.schemas[key]) { //if already exists, we have a conflict
+                throw new Error("$def already exists in schemas " + key);
+            }
+            spec.components.schemas[key] = component.document["x-$defs"][key]
+        });
+        delete component.document["x-$defs"];
+    }
+}
+
 const createDefinitions = function (components) {
     const json = readFile(definitionsTemplatePath);
     let spec = JSON.parse(json);
 
     for (const component of components) {
-        spec.definitions[component.name] = component.document;
+        moveDefsToCommponentsSchemas(spec, component)
+        spec.components.schemas[component.name] = component.document;
     }
+
     return spec;
 }
 
--- tools/jsonschema2openapi/processor.js
+++ tools/jsonschema2openapi/processor.js
@@ -35,22 +35,35 @@ function toPascalCase(str) {
 /**
  * the objective is to consolidate all these types to a common "definitions.js"
  * This will replace the JSON Schema $ref values with a local reference
- * so we can do the following in the file that defines the paths: './definitions.json#/definitions/<Component>'
+ * so we can do the following in the file that defines the paths: './definitions.json#/components/schemas/<Component>'
  * @param {object} component
  */
 function replaceRefs(component) {
+    // process references INSIDE $ref
     JSONPath.apply(component.document, '$..["$ref"]', (value) => {
+        // replace HTTP reference to a JSON Schema with the local reference of the openapi component
         if (value.startsWith('http')) {
-            const schemaUrl = new SchemaUrl(value);
-            return `#/definitions/${component.name}`
+            return `#/components/schemas/${component.name}`
         }
 
+        // replace what was a $def local json schema reference with
+        // the "x-$defs" path (which is done during conversion)
         if (value.indexOf('$defs') != -1) {
-            return value.replace('$defs', 'x-$defs');
+            return value.replace('$defs', 'components/schemas');
         }
 
         return value;
     });
+
+    if (component.document["x-$defs"]) {
+        //clean up x-$defs which is a conversion of references to the JSON Schemas
+        //remove the HTTP json schema objects here since they will get referenced
+        //locally
+        const orphanedExternalKeys = Object.keys(component.document["x-$defs"]).filter(k => k.startsWith("http"));
+        orphanedExternalKeys.forEach(key => {
+            delete component.document["x-$defs"][key];
+        });
+    }
 }
 
 class JsonSchemaProcessOptions {
