eb49a32050da73aeb918d05947b988650f50de05
Kevin Hillinger <kevin.hillinger@microsoft.com>
2024-01-17 13:02:58 -0500

    fixing dependency on pydantic and making it compatible with a specific v2 version.

--- partnercenter/setup.py
+++ partnercenter/setup.py
@@ -11,54 +11,50 @@ from codecs import open
 from setuptools import setup, find_packages
 
 
-NAME = 'partnercenter'
+NAME = "partnercenter"
 
 # Version extraction inspired from 'requests'
-with open(os.path.join('azext_partnercenter', 'version.py'), 'r') as fd:
-    VERSION = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
+with open(os.path.join("azext_partnercenter", "version.py"), "r") as fd:
+    VERSION = re.search(
+        r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
+    ).group(1)
 
 if not VERSION:
-    raise RuntimeError('Cannot find version information')
+    raise RuntimeError("Cannot find version information")
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Intended Audience :: System Administrators',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'License :: OSI Approved :: MIT License',
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Intended Audience :: System Administrators",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "License :: OSI Approved :: MIT License",
 ]
 
 # TODO: Add any additional SDK dependencies here
-DEPENDENCIES = [
-    'docker',
-    'azure-storage-blob',
-    'requests',
-    'pydantic<2'
-]
+DEPENDENCIES = ["docker", "azure-storage-blob", "requests", "pydantic ~= 2.5.3"]
 
-with open('README.rst', 'r', encoding='utf-8') as f:
+with open("README.rst", "r", encoding="utf-8") as f:
     README = f.read()
-with open('HISTORY.rst', 'r', encoding='utf-8') as f:
+with open("HISTORY.rst", "r", encoding="utf-8") as f:
     HISTORY = f.read()
 
 setup(
     name=NAME,
     version=VERSION,
-    description='Microsoft Azure CLI Extension for Partner Center',
-    author='Microsoft Corporation',
-    author_email='azpycli@microsoft.com',
-    url='https://github.com/Azure/partnercenter-cli-extension/tree/main/partnercenter',
-    long_description=README + '\n\n' + HISTORY,
-    license='MIT',
+    description="Microsoft Azure CLI Extension for Partner Center",
+    author="Microsoft Corporation",
+    author_email="azpycli@microsoft.com",
+    url="https://github.com/Azure/partnercenter-cli-extension/tree/main/partnercenter",
+    long_description=README + "\n\n" + HISTORY,
+    license="MIT",
     classifiers=CLASSIFIERS,
     packages=find_packages(),
     install_requires=DEPENDENCIES,
-    package_data={'azext_partnercenter': ['azext_metadata.json']},
+    package_data={"azext_partnercenter": ["azext_metadata.json"]},
 )
