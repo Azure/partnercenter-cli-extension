708282979ad48f951bf744c73c26cb1c3bbbab2b
Kevin Hillinger <kevin.hillinger@microsoft.com>
2024-01-25 10:54:28 -0500

    fix ref to modm wheel to now use pypi

--- partnercenter/setup.py
+++ partnercenter/setup.py
@@ -42,7 +42,7 @@ DEPENDENCIES = [
     "azure-storage-blob",
     "requests",
     "pydantic ~= 2.5.3",
-    "commercial-marketplace-offer-deploy @ https://github.com/microsoft/commercial-marketplace-offer-deploy/releases/download/v2.0.0/commercial_marketplace_offer_deploy-2.0.0-py3-none-any.whl#sha256=2a57dbdc6e59eccd9c2c4421c8385060004030b4cc48dd95d69dcb050df85149",
+    "commercial-marketplace-offer-deploy",
 ]
 
 with open("README.rst", "r", encoding="utf-8") as f:
