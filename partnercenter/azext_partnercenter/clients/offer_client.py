d3a51826e7d80244e6576ead7b7a1c60ab988ba6
Kevin Hillinger <kevin.hillinger@microsoft.com>
2025-04-18 11:34:03 -0400

    marking methods as removed

--- partnercenter/azext_partnercenter/clients/offer_client.py
+++ partnercenter/azext_partnercenter/clients/offer_client.py
@@ -72,12 +72,14 @@ class OfferClient(BaseClient):
             resource=Resource(durable_id=product.id, type=product.resource_type)
         )
 
+    # removed
     def get_setup(self, offer_external_id):
         offer = self.get(offer_external_id)
         result = self._sdk.product_client.products_product_id_setup_get(offer.resource.durable_id, self._get_access_token())
         print(result)
         return self._map_setup(offer_external_id, result)
 
+    # removed
     def update_setup(self, parameters: OfferSetup):
         offer = self.get(parameters.id)
 
@@ -107,6 +109,7 @@ class OfferClient(BaseClient):
         offer_setup = self.get_setup(parameters.id)
         return offer_setup
 
+    # removed
     def _map_setup(self, offer_external_id, api_setup: MicrosoftIngestionApiModelsProductsAzureProductSetup) -> OfferSetup:
         reseller = (next((x for x in api_setup.channel_states if x['type'] == "Reseller"), None))['value'] == 'Enabled'
         sell_through_microsoft = api_setup.selling_option == 'ListAndSell'
